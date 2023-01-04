7a3920a8dcbfbc14236ceec9c056238ce0e226f4
Cunha <cunhanik1@gmail.com>
2023-01-04 17:50:58 +0000

    update

---
+++ docs/React/11 Class com State/class-with-state/src/components/planets/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Planet from "./planet";
+
+const Planets = () => {
+  return (
+    <>
+      <h3>Planet List: </h3>
+      <hr />
+      <Planet />
+      <Planet />
+      <Planet />
+    </>
+  );
+};
+
+export default Planets;
---
+++ docs/React/11 Class com State/class-with-state/src/components/shared/gray_img/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const GrayImg = () => {
+  return (
+    <img
+      className="gray-img"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/420px-Mercury_in_color_-_Prockter07-edit1.jpg"
+    />
+  );
+};
+
+export default GrayImg;
